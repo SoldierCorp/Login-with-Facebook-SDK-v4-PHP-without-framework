d1b2fe13e34ad4b767d24636fad87a34faf4d0b5
José Ignacio <joseiogr@gmail.com>
2015-02-02 18:04:22 +0100

    Updating scope to be able of get the user email

--- config/facebook.php
+++ config/facebook.php
@@ -3,5 +3,5 @@
 $config = array(
 	'app_id' => '347956232028939',
 	'app_secret' => '91858566cca749b14d952c97c554af07',
-	'scopes' => array('email','read_friendlists','user_online_presence')
+	'scopes' => array('scope' => 'email,read_friendlists,user_online_presence')
 );
